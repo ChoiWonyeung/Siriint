52538a411c1af719d720570000dcdbb6f8bf89dd
Kangnam Kim <kangnam@coconutsilo.com>
2021-07-22 10:17:46 +0900

    20210722 김강남 코머신1 뎁스 완료

--- Markets/Komachine/komachine_1depth.py
+++ Markets/Komachine/komachine_1depth.py
@@ -2,9 +2,10 @@ from Modules.common import *
 import komachine_config as config
 
 
-def crawl_key(sample=False):
+def crawl_key(sample=False, save=False):
     # 1차 크롤링 리스트
     source = []
+    dic_komachine = {}
 
     # 1차 크롤링 [url]
     page = 1
@@ -18,10 +19,17 @@ def crawl_key(sample=False):
         else:
             if soup.select(config.selector_source) == []:
                 break
-    return source
 
-if __name__ == '__main__':
-    source = crawl_key()
+    soup = req_bsEncoding(config.url_robot)
+    sleep_random(2, 4, print_time=True)
+    source_ls = soup.select('div.wrapper > a')
+    source += [i['href'] for i in source_ls]
+    for key in source:
+        dic_komachine[key] = {}
+    if save == True:
+        json_save('Bigwave-Robotics/Markets/Komachine/json/komachine_1depth.json', dic_komachine)
+    pickle_save('Bigwave-Robotics/Markets/komachine/pickles/komachine_1depth.pkl', dic_komachine)
+    return dic_komachine
 
 # soup = req_bsEncoding('https://www.komachine.com/ko/companies/abb/products/')
 # div_product = soup.select('section.company-products a.item.product')
