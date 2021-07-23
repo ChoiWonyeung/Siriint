b193611208e8ff55d57aff6c9c2c3081b0552b04
Kangnam Kim <kangnam@coconutsilo.com>
2021-07-23 12:00:29 +0900

    20210723(금) 김강남 brandsort 작성완

--- BrandSort.py
+++ BrandSort.py
@@ -1,4 +1,3 @@
-from glob import glob
 import pandas as pd
 from Modules.common import *
 
@@ -10,28 +9,23 @@ def brand_make():
     brand_ls = brand_xl['이름'].tolist()
     return brand_ls
 
-def daara_key(brand_ls):
-    daara_raw = json_load('/Users/kimkangnam/PycharmProjects/CompanyProject/DataVoucher/Bigwave-Robotics/makeExcel(full_length)/daara_result__.json')
-    daara_keylist = list(daara_raw.keys())
-    # for brand in brand_ls:
-    #     globals()['brandDic_{}'.format(brand)] = {}
+
+def market_key(brand_ls, file='/Users/kimkangnam/PycharmProjects/CompanyProject/DataVoucher/Bigwave-Robotics/makeExcel(full_length)/daara_result__.json'):
+    dic_raw = json_load(file)
+    ls_marketKey = list(dic_raw.keys())
 
     a = []
-    for key in daara_keylist:
-         a.append(daara_raw[key]['brand'])
+    for key in ls_marketKey:
+         a.append(dic_raw[key]['brand'])
 
-    # b = []
-    # for i in a:
-    #     if i not in brand_ls:
-    #         b.append(i)
-
-    dic = {}
+    dic_brand = {}
     for i in sorted(brand_ls):
-        dic[i] = {}
-        dic[i][0] = i
-    return dic, daara_raw
+        dic_brand[i] = {}
+        dic_brand[i][0] = i
+    return dic_brand, dic_raw
 
-def normalize(dic):
+
+def dictionary_normalize(dic):
     dic['YASKAWA'][1] = '야스카와'
     dic['AUBO'][1] = '아우보'
     dic['Panasonic'][1] = '파나소닉'
@@ -44,34 +38,56 @@ def normalize(dic):
     dic['KUKA'][1] = '쿠카'
     return dic
 
-if __name__ == '__main__':
-    from pprint import pprint
-    brand_ls = brand_make()
-    dic, daara_raw = daara_key(brand_ls)
-    dic_normalized = normalize(dic)
 
-    i = list(daara_raw.keys())[0]
-
-    pprint(dic_normalized)
-    goal = '아우보'
-
-    delete_keys = []
-    temp_dict = dict()
+def dictionary_invert(dic_normalized):
+    dic_inverted = dict()
     for key1, inner_dict in dic_normalized.items():
+        for value in inner_dict.values():
+            dic_inverted[value] = key1
+    return dic_inverted
+
+
+def brand_sorting(dic_raw, dic_brandInverted):
+    dic_new = {}
+    for key in list(dic_raw.keys()):
+        if dic_raw[key]['brand'] in dic_brandInverted.keys():
+            brand = dic_raw[key]['brand']
+            dic_new[key] = dic_raw[key]
+            dic_new[key]['brand'] = dic_brandInverted[brand]
+    return dic_new
+
+
+def main():
+    brand_ls = brand_make()
+    dic_brand, dic_raw = market_key(brand_ls)
+    dic_brandNormalized = dictionary_normalize(dic_brand)
+    dic_brandInverted = dictionary_invert(dic_brandNormalized)
+    dic_new = brand_sorting(dic_raw, dic_brandInverted)
+    return dic_new
+
+
+if __name__ == '__main__':
+    dic_new = main()
+
+
+
+
+
         # goal이 안쪽 딕셔너리의 value로 존재하면
-        if goal in inner_dict.values():
-            # temp dict에 key를 goal로 해서 inner dict를 복사
-            temp_dict[goal] = inner_dict
-        else:
-            # 존재하지 않으면 기존키 (key1) 유지
-            temp_dict[key1] = inner_dict
 
-    # 새로운 딕셔너리로 덮어쓰기
-    dic_normalized = temp_dict
-    for key in delete_keys:
-        del dic_normalized[key]
 
-    pprint(dic_normalized)
+
+    #     if goal in inner_dict.values():
+    #         # temp dict에 key를 goal로 해서 inner dict를 복사
+    #         temp_dict[goal] = inner_dict
+    #     else:
+    #         # 존재하지 않으면 기존키 (key1) 유지
+    #         temp_dict[key1] = inner_dict
+    #
+    # # 새로운 딕셔너리로 덮어쓰기
+    # dic_normalized = temp_dict
+    # for key in delete_keys:
+    #     del dic_normalized[key]
 
     # for i in dic.values():
     #     print(i.values())
